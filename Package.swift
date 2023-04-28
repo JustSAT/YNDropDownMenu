fb7757b904118a86f08cdea4d6cdcf7cd67c3cf1
Artem Shevchenko <avtobot.08@gmail.com>
2023-04-28 11:31:11 +0300

    Added SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "YNDropDownMenu"
+    name: "YNDropDownMenu",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "YNDropDownMenu", targets: ["YNDropDownMenu"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "YNDropDownMenu",
+            path: "YNDropDownMenu"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
